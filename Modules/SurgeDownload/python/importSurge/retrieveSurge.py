271cea6492bb9539d852e7d39563685e37c5a954
mandi thran <m.thran@unsw.edu.au>
2021-02-10 12:34:28 +1100

    Retrieve storm surge file, rounded down to nearest 6 hr int. of system time

--- Modules/SurgeDownload/python/importSurge/retrieveSurge.py
+++ Modules/SurgeDownload/python/importSurge/retrieveSurge.py
@@ -1,5 +1,16 @@
 import numpy as np
 
+def round_hours(dt, resolutionInHours):
+    """round_hours(datetime, resolutionInHours) => datetime rounded to lower interval
+    Works for hour resolution up to a day (e.g. cannot round to nearest week).
+    """
+    from datetime import datetime, timedelta
+    # First zero out minutes, seconds and micros
+    dtTrunc = dt.replace(minute=0,second=0, microsecond=0)
+    # Figure out how many minutes we are past the last interval
+    excessHours = (dtTrunc.hour) % resolutionInHours
+    # Subtract off the excess minutes to get the last interval
+    return dtTrunc + timedelta(hours=-excessHours)
+
 def retrieveSurgeFile():
-    s = pd.Series([1,3,5,np.nan,6,8])
-    return("Hello surge %s" % s)
+    return("Hello surge...")
