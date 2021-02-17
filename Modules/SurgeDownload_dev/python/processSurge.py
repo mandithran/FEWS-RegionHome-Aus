fcf5cf3204f0048f7c1baaf9ea8f30802be8d53d
mandi thran <m.thran@unsw.edu.au>
2021-02-17 11:47:20 +1100

    configure ImportAusSurgeAdapter

--- Modules/SurgeDownload_dev/python/processSurge.py
+++ Modules/SurgeDownload_dev/python/processSurge.py
@@ -1,107 +1,58 @@
+# The BOM national storm surge system has a forecast horizon of 3 days.
+
+# Water levels should start one day before desired start of
+# forecast window to allow for spin-up of xbeach
+
+#TODO: Turn coordinates into dimensions
+
 # =================== Import Modules =================== #
 import netCDF4 as nc
-import numpy as numpy
+import numpy as np
 import os
-import matplotlib.pyplot as plt
+import warnings
+import xarray as xr
+import pandas as pd
+import re
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-# =================== Paths =================== #
+# =================== Variables =================== #
 # Relative to Region Home, since that is where executable is called from
 importDir = ".\Import\AusSurge"
-fname = 'IDZ00154_StormSurge_national_2020122400.nc'
-importPath = os.path.join(importDir,fname)
+fname = 'IDZ00154_StormSurge_national_2021021118.nc'
+site = "Narrabeen"
 
-import numpy as np
-import netCDF4 as nc
+# =================== Paths =================== #
+ifile = os.path.join(importDir,fname)
 
-def readNetcdf(ncObj, varname='surge', verbose=False):
-  '''
-  context: 
-      assume dims are (time, x, y) or (point,time)
-      regular grid or point array only
-  '''
-  
-  varObj  = ncObj.variables[varname]
-  dims    = varObj.dimensions
-  allVars = ncObj.variables.keys()
+# Lat/lon - for selecting the best point for the timeseries
+# These points need to be determined ahead of time by looking at nc file
+d = {"Narrabeen": {'lat': -33.71775, 'lon': 151.32191},
+     "Mandurah": {'lat': -32.508728, 'lon': 115.72912}}
 
-  # try to identify coords
-  try:
-      varY,varX = varObj.coordinates.strip().split(' ')            # mixed-bag as to how this meta data is used: 'lat lon' or 'lon lat'   
-  except:
-      print("!! couldnt use coorindates metadata")
-      print(varObj)
-      varY = 'lat'
-      varX = 'lon'
+# Take a look at existing coordinates, dimensions, and variables
+ds = xr.open_dataset(ifile)
+print("NetCDF file metadata:")
+print(ds.keys)
 
-  if (('y'   in (varX).lower()) and ('x'   in (varY).lower())) or \
-     (('lat' in (varX).lower()) and ('lon' in (varY).lower())):
-         print("!! looks like coordinate metadata is mis-ordered ...try to flip!!")
-         oldX = varX; oldY = varY
-         varY = oldX; varX = oldY
-  if verbose:  
-      print("x/y coord names from metadata: ", varX,varY)
+# ds.keys will reveal that the NetCDF file does not have lat/lon dimensions - it has a "point" and "time" dimensions
+# These "point" dimensions are essentially the index of a given point
+# The "point dimension" has the same shape as the lat and lon fields
+# This is because this is not a regular grid; each point has a unique lat, lon value
+# The following will return the index that matches the lat/lon point of interest
+# This index allows for selection of subsets of the xarray
+df = pd.DataFrame({"lon":ds.coords['lon'].values,"lat":ds.coords['lat'].values})
+ind = df[(df['lon']==d[site]["lon"]) & 
+        (df['lat']==d[site]["lat"])].index
 
-  # time
-  if   len(dims)==3:
-      varT  = varObj.dimensions[0]  # assume time is 1st and varname==dimname
-  elif len(dims)==2:
-      varT  = varObj.dimensions[1]  # assume time is 2nd and varname==dimname
-  else:
-      print("!! problem with assumed data shape !!")
-      sys.exit(1)
+# Select surge timeseries for the point of interest
+surge = ds.surge[ind].values
 
-  # check   
-  if (varX not in allVars) or (varY not in allVars):
-      if verbose:  
-          print("!! override x/y coord names with guesses")
-      varX = 'lon'    # guess
-      varY = 'lat'    # guess
-  if (varT not in allVars):
-      if verbose:  
-          print("!! override t coord names with guesses")
-      varT = 'Time'   # guess
-
-  # read array
-  data = ncObj.variables[varname]
-  X    = ncObj.variables[varX]     
-  Y    = ncObj.variables[varY]
-  T    = ncObj.variables[varT]
-
-  # spatial slice  
-  if len(dims)==3:
-      data_slice = data[0,:,:]
-  elif len(dims)==2:
-      data_slice = data[:,0]
-
-  # wet mask
-  if 'wet' in allVars:
-      wet  = ncObj.variables['wet']
-      if len( wet.dimensions ) != 2:
-          wet = wet[0,:,:]
-  elif 'sea_lnd_ice_mask' in allVars:
-      wet  = (ncObj.variables['sea_lnd_ice_mask'][:,:] == 0).astype("f")     # 0=ocean, 1=ice, 2=land    ! sic = want float, not int
-  else:
-      try:
-          mask = data_slice.mask
-      except:
-          if verbose:  
-              print("no mask field, derive...")
-          try:
-              vmin,vmax = data.valid_range
-              if verbose:  
-                  print("valid minmax", vmin,vmax)
-                  print("mask field from valid_range attr")
-              data_masked = np.ma.masked_outside( data_slice, vmin, vmax)
-              mask = data_masked.mask
-          except:
-              if verbose:  
-                  print("no mask info at all ...assume all points are valid")
-              mask = np.zeros( data_slice.shape )
-      # ...derive wet from land/sea 
-      wet = np.abs((mask-1))*1.0
-
-  # note order
-  return T,Y,X,data,wet
-
-ds = nc.Dataset(importPath)
-time,y,x,data,wet = readNetcdf(ds)
+# Create new pandas df to export timeseries
+dfOut = pd.DataFrame({"time":ds.coords['time'].values,
+                    "surge (m)":surge[0]
+                    })
+                    
+# File name based on site and date time given in original nc file
+dateTime = re.split("[_.]", fname)[3]
+ofname = "nss_%s_%s.csv" % (site, dateTime)
+dfOut.to_csv(os.path.join(importDir,ofname),index=False)
