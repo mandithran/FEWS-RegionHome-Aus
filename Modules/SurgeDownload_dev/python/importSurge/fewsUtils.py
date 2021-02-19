035e41d85642274e2b0269a8f970a7dee8426ca4
mandi thran <m.thran@unsw.edu.au>
2021-02-19 15:10:54 +1100

    Run full retrieve, process, import workflow from FEWS

--- Modules/SurgeDownload_dev/python/importSurge/fewsUtils.py
+++ Modules/SurgeDownload_dev/python/importSurge/fewsUtils.py
@@ -1,3 +1,14 @@
+def clearDiagLastLine(diagFile=None):
+    fd=open(diagFile,"r")
+    d=fd.read()
+    fd.close()
+    m=d.split("\n")
+    s="\n".join(m[:-1])
+    fd=open(diagFile,"w+")
+    for i in range(len(s)):
+        fd.write(s[i])
+    fd.close()
+
 def write2DiagFile(errorLevel=None, note=None):
-    string = "<line level=\"%s\" description=\"FROM PYTHON: %s\"/>\n" % (errorLevel, note)
+    string = "\n<line level=\"%s\" description=\"FROM PYTHON: %s\"/>\n" % (errorLevel, note)
     return string
