1b82b207101597a1b91401945196d3c29dd67f31
mandi thran <m.thran@unsw.edu.au>
2021-03-08 12:31:16 +1100

    Restore binaries and Module files

---
+++ Modules/WaveDownload_dev/python/process-waves.py
@@ -0,0 +1,229 @@
+# Modules
+import traceback
+import sys
+import os
+import shutil
+from importWaves import fewsUtils
+from importWaves import retrieveWaves
+import xarray as xr
+import pandas as pd
+from datetime import datetime
+
+import numpy as np
+import netCDF4
+import geopandas as gpd
+
+def main(args=None):
+    """The main routine."""
+    args = [a for a in sys.argv[1:] if not a.startswith("-")]
+
+    #============== Parse arguments from FEWS ==============#
+    regionHomeDir = "C:\\Users\\z3531278\\Documents\\01_FEWS-RegionHome-Aus"
+    workDir = "C:\\Users\\z3531278\\Documents\\01_FEWS-RegionHome-Aus\\Modules\\WaveDownload_dev"
+    siteName = "Narrabeen"
+    sysTime = "20210228_0000"
+    locSetFilename = "surgeLocations.csv"
+    jonSwapParam = 3.3
+    dirSpread = 10.
+    dtbc = 1.
+
+    #============== Paths ==============#
+    modulePath = os.path.join(regionHomeDir,"Modules")
+    diagBlankFile = os.path.join(workDir,"diagOpen.txt")
+    diagFile = os.path.join(workDir,"diag.xml")
+    xbWorkDir = os.path.join(modulePath,".\XBeach",siteName,"runs",siteName,"workDir")
+
+    #============== Create directories if they don't exist ==============#
+    if not os.path.exists(xbWorkDir):
+        os.makedirs(xbWorkDir)
+
+    #============== Write out initial commands to diag file ==============#
+    # Copy and rename diagOpen.txt
+    shutil.copy(diagBlankFile,diagFile)
+
+    with open(diagFile, "a") as fileObj:
+        currDir = os.getcwd()
+        fileObj.write(fewsUtils.write2DiagFile(3,
+            "Command-line arguments sent to python script from FEWS: %s" 
+            % (str(args)) ))
+        fileObj.write(fewsUtils.write2DiagFile(3,"Current directory: %s" %currDir))
+        fileObj.write(fewsUtils.write2DiagFile(3,"If Python error exit code 1 is triggered, see exceptions.log file in Module directory."))
+        fileObj.write("</Diag>")
+
+
+    #============== Read in Auswave mesh point locations ==============#
+    # "Points of interest"
+    ifile = os.path.join(workDir, "auswaveOutPts_Narrabeen.csv")
+    df = pd.read_csv(ifile)
+    gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.lon, df.lat))
+    ausWaveEPSG = int(4939)
+    gdf.set_crs(epsg=ausWaveEPSG)
+
+    #============== Load Location Set ==============#
+    locSetPath = os.path.join(regionHomeDir, "./Config/MapLayerFiles", locSetFilename)
+    df = pd.read_csv(locSetPath)
+
+    #============== Parse system time ==============#
+    # TODO make a full function for parsing system time in fews utils
+    systemTime = datetime(int(sysTime[:4]),
+                          int(sysTime[4:6]),
+                          int(sysTime[6:8]),
+                          hour=int(sysTime[9:11]))
+
+    roundedTime = retrieveWaves.round_hours(systemTime, 6)
+
+    #============== Load Location Set ==============#
+    locSetPath = os.path.join(regionHomeDir, "./Config/MapLayerFiles", locSetFilename)
+    df = pd.read_csv(locSetPath)
+
+
+    # TODO: Import location set and use it to parse filename
+    #============== Parse BOM file name  ==============#
+    bomDate = str(str(roundedTime.year)+
+            str(roundedTime.month).zfill(2)+
+            str(roundedTime.day).zfill(2))
+    bomTime = str(str(roundedTime.hour).zfill(2)+
+            str(roundedTime.minute).zfill(2))
+    bomDT = str(bomDate+"T"+bomTime+"Z")
+    cityCode = df.loc[df["Name"]==siteName, "City_code"].iloc[0]
+    fname = "%s.msh.%s.nc" % (cityCode,bomDT)
+    print(fname)
+
+    #============== Read in Auswave output ==============#
+    ausWavedf = os.path.join(workDir, "ncFiles", fname)
+    ds = xr.open_dataset(ausWavedf)
+
+    #============== Slice it to a 3-day forecast for now ==============#
+    startTime = min(ds.time[:].values)
+    endTime = startTime + np.timedelta64(3,"D")
+    ds =ds.sel(time=slice(startTime,endTime))
+
+    #============== Keep only the mesh points of interest ==============#
+    # Convert xarray dataset to geodataframe
+    dfPts = pd.DataFrame({"Lon":ds.longitude.values,
+                        "Lat":ds.latitude.values})
+    gdfPts = gpd.GeoDataFrame(dfPts, geometry=gpd.points_from_xy(dfPts.Lon,dfPts.Lat))
+    gdfPts.set_crs(epsg=4939)
+    from shapely.ops import nearest_points
+    # unary union of the geomtries 
+    pts = gdfPts.geometry.unary_union
+    # Find the nearest geometries in Auswave output to "points of interest"
+    # Return the old Auswave index value for slicing Auswave output
+    def nearGeom(point, pts=None, gdfIn=None, outVar=None):
+        # find the nearest point and return the corresponding Place value
+        nearest = gdfIn.geometry == nearest_points(point, pts)[1]
+        geometries = gdfIn[nearest].geometry.to_numpy()[0]
+        indeces = gdfIn[nearest].index.to_numpy()[0]
+        if outVar == "index":
+            return indeces
+        elif outVar == "geometries":
+            return geometries
+        else:
+            raise
+    gdf["oldIndex"] = gdf.apply(lambda row: nearGeom(row.geometry, pts=pts, gdfIn=gdfPts, outVar="index"), axis=1)
+    indArr = gdf["oldIndex"].to_numpy()
+    ds = ds.isel(node=indArr)
+
+    #============== Get timestep of timeseries in seconds ==============#
+    t2 = ds.coords["time"][1].values
+    t1 = ds.coords["time"][0].values
+    dt = t2-t1
+    dt = dt.astype('timedelta64[s]').astype(int).astype(float)
+
+    #============== Loop through and generate wavefiles ==============#
+    roundDec = 3
+    for index in np.arange(0,ds.sizes['node']):
+        dfOut = pd.DataFrame({"Hsig":ds.hs[:,0].values.round(roundDec),
+                             "Tp":ds.tp[:,0].values.round(roundDec),
+                             "dir":ds.dir[:,0].values.round(roundDec),
+                             "JS":jonSwapParam,
+                             "dirSpread":dirSpread,
+                             "dt_sec":dt.round(roundDec),
+                             "dtbc":dtbc})
+        ofileName = "wavefile%s.txt" % (index+1)
+        dfOut.to_csv(os.path.join(xbWorkDir,ofileName), sep='\t', header=False, index=False)
+
+
+    #============== Add wavefile name as gdf entry ==============#
+    gdf["ind"] = gdf.index.astype(int)
+    gdf["wavefile"] = [f"wavefile{i}.txt" for i in gdf.index]
+
+    
+    #============== Generate input file that directs XBeach to wavefiles ==============#
+    # Load XBeach mesh as geodataframe - netCDF format
+    xbf = os.path.join(modulePath,".\XBeach",siteName,"inputFiles\\bathyTopo","xbNarra_zb_2021.nc")
+    xbds = xr.open_dataset(xbf)
+    ncolXB = xbds.ncols
+    nrowXB = xbds.nrows
+
+    # Convert to geodataframe to do point searching
+    xbgdf = xbds.to_dataframe()
+    xbgdf = gpd.GeoDataFrame(xbgdf, geometry=gpd.points_from_xy(xbgdf.globalx,xbgdf.globaly))
+    xbEPSG = int(xbds.epsg)
+    xbgdf.set_crs(xbEPSG)
+
+    # Using points of interest (now in ds), find closest points on XBeach mesh,
+    # return value of index in XBeach output
+    # Reproject points to projection used in XBeach
+    gdf_proj = gdf.copy()
+    gdf_proj.set_crs(epsg=ausWaveEPSG, inplace=True)
+    gdf_proj['geometry'] = gdf_proj['geometry'].to_crs(epsg=xbEPSG)
+    # unary union of the geomtries 
+    pts = xbgdf.geometry.unary_union
+    gdf_proj["xbIndex"] = gdf_proj.apply(lambda row: 
+                     nearGeom(row.geometry, pts=pts, gdfIn=xbgdf, outVar="index"), axis=1)
+    
+    # Grab the index in the relevant row, first column
+    # Column indeces go up starting from seaward points
+    # Split xbeach indeces into separate rows, cols
+    indList = gdf_proj.xbIndex.tolist()
+    gdf_proj[["xbcol","xbrow"]] = pd.DataFrame(indList, index=gdf_proj.index)
+    gdf_proj['targetCol'] = 0 # First column, starting from seaward boundary
+    gdf_proj['targetRow'] = gdf_proj.xbrow
+    print(gdf_proj)
+
+    # Grab relevant geometries from XBeach grid using target indeces
+    # Slice by desired cols first
+    #gdf_proj['targetX'] = xbgdf.loc[0]
+    targetColArr = gdf_proj['targetCol'].values
+    targetRowArr = gdf_proj['targetRow'].values
+    targetInd = zip(targetColArr,targetRowArr)
+    xbgdf_subset = xbgdf.loc[targetInd]
+    xbgdf_subset['xtarget'] = xbgdf_subset.geometry.x
+    xbgdf_subset['ytarget'] = xbgdf_subset.geometry.y
+    # Round vals
+    xbgdf_subset = xbgdf_subset.round({'xtarget':2,'ytarget':2})
+    # Bring old index over for merging
+    xbgdf_subset['ind'] = gdf_proj['ind'].values
+    print(xbgdf_subset)
+    
+    # Merge dfs
+    df = pd.merge(gdf_proj,xbgdf_subset,on="ind")
+    print(df)
+
+    # Send to txt file
+    df.to_csv(os.path.join(xbWorkDir, "loclist.txt"), 
+              columns=["xtarget","ytarget","wavefile"],
+              header=False,sep=' ', index=False)
+
+    
+    #============== Load Location Set ==============#
+    #locSetPath = os.path.join(regionHomeDir, "./Config/MapLayerFiles", locSetFilename)
+    #df = pd.read_csv(locSetPath)
+
+    #============== Grab most recently downloaded nc file ==============#
+    # Uses a function from processSurge.py
+    #downloadDir = os.path.join(workDir,"ncFiles")
+    #newestNC = processSurge.getMostRecentFile(downloadDir=downloadDir)
+
+
+
+## If Python throws an error, send to exceptions.log file
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        with open("exceptions.log", "w") as logfile:
+            logfile.write(str(e))
+            logfile.write(traceback.format_exc())
+        raise
