c5548562c2a89a2baee8be42855a8dde7ecc101f
mandi thran <m.thran@unsw.edu.au>
2021-03-09 16:55:35 +1100

    Add matlab script that extracts astrotides to csv

---
+++ Data/Tides/code/subsetTide.m
@@ -0,0 +1,16 @@
+tide = load('..\raw\SYDNEY_ASTRO_TIDE_1979_2050.mat')
+
+index1 = 700000; %1998
+index2 = 1800000; %2030
+
+time_AEST2 = datetime(tide.tide.time_AEST(1,index1:index2),'ConvertFrom','datenum');
+astro_tide2 = tide.tide.astro_tide(1,index1:index2);
+
+time_AEST2 = transpose(string(time_AEST2));
+astro_tide2 = transpose(double(astro_tide2));
+
+arr = [time_AEST2, astro_tide2];
+
+header = {"Time (dd/mm/yyyy HH:MM AEST)","Elevation (m AHD)"};
+output = [header; num2cell(arr)];
+writecell(output,'..\processed\SydneyTidesAEST.csv');
