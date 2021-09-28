9081559bfe9f4ab6fbae7961dda502c016b407e7
mandi thran <m.thran@unsw.edu.au>
2021-09-28 14:08:30 +1000

    Minor changes - wrl desktop

--- Scripts/runForecast/run_forecast_loop.bat
+++ Scripts/runForecast/run_forecast_loop.bat
@@ -1,30 +1,40 @@
-rem Run: START /B C:\Users\mandiruns\Documents\01_FEWS-RegionHome-Aus\Scripts\runForecast\run_forecast_loop_1.bat > forecastLog_1.txt 2>&1
-rem arguments to test/debug: C:\Users\mandiruns\Documents\01_FEWS-RegionHome-Aus\ 2020-02-07 00:00 2020-02-07 12:00
+rem #=========================== Region Home ===========================#
+rem Location of the Region Home directory
 set regionHome=C:\Users\mandiruns\Documents\01_FEWS-RegionHome-Aus\
+
+rem #=======================+==== Log file ===================++========#
+rem Location and name of the log file for the group of forecasts/hindcasts
 set logFile=%regionHome%\\forecastLog_MCCALLUM1.txt
 
+rem #================= BOM Storm Surge Forecasts Location ==============#
 rem Location if grabbing files from WRL1, keep the forward slashes or else it doesn't work
 rem set surgeForecastLoc = "//ad.unsw.edu.au/OneUNSW/ENG/WRL/WRL1/Coastal/Data/Tide/WL_Forecast/BOM_Storm_Surge/raw/corrected"
 rem # Location if grabbing file from a local folder
 set surgeForecastLoc=%regionHome%/ExternalForecasts/BOM/surge
 
+rem #===================== BOM Wave Forecasts Location =================#
 rem Location if grabbing files from WRL1, keep the forward slashes or else it doesn't work
 rem set waveForecastLoc = "//ad.unsw.edu.au/OneUNSW/ENG/WRL/WRL1/Coastal/Data/Wave/Forecast/BOM_products/BOM_nearshore_wave_transformation_tool/raw/Mesh"
 rem Location if grabbing file from a local folder
 set waveForecastLoc=%regionHome%/ExternalForecasts/BOM/waves
 
-rem Format: YYYY-MM-DD
+rem #=========== Dates and Times for Batch of Hindcast Runs ============#
+rem Start date of hindcasts, format: YYYY-MM-DD
 set systemStartDate=2020-01-01 
-rem Format: HH:MM
+rem Start time of hindcasts, format: HH:MM
 set systemStartTime=12:00
 rem Format: YYYY-MM-DD
 set systemEndDate=2020-02-11
 rem Format: HH:MM
 set systemEndTime=00:00
 
+rem #================== Anaconda virtual environment ===================#
+rem Specify the path
 set CONDAPATH=C:\\Users\\mandiruns\\Anaconda3
 set ENVPATH=%regionHome%\\bin\\windows\\python\\bin\\conda-venv
-
+rem Call the script that activates the virtual environment
 call %CONDAPATH%\Scripts\activate.bat %ENVPATH% >> %logFile%
+
+rem #==================== Run run_forecast_loopy.py ====================#
 "python" "C:\Users\mandiruns\Documents\01_FEWS-RegionHome-Aus\Scripts\runForecast\run_forecast_loop.py" %regionHome% %systemStartDate% %systemStartTime% %systemEndDate% %systemEndTime% %surgeForecastLoc% %waveForecastLoc% >> %logFile%
 pause
